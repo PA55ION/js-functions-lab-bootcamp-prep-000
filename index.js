c614c85ea47578fe7e09bb99002d9157afb2d6ab
Phakarat Campbell <jirallc@gmail.com>
2018-07-04 15:29:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,17 +4,17 @@ function happyHolidays() {
 }
 
 function happyHolidaysTo(name) {
-  return `Happy holidays, ${name}`;
+  return `Happy holidays, ${name}!`;
 }
 
 function happyHolidayTo(holiday, name) {
   return `Happy ${holiday}, ${name}!`
 }
 
-function holidayCountdown(holiday, days) {
+function holidayCountdown(days, holiday) {
   return `It's ${days} days until ${holiday}`
 }
 
 happyHolidaysTo('Jackie');
 happyHolidayTo('New Year', 'Babe');
-holidayCountdown('28', 'Christmas');
+holidayCountdown("20", "Mother's");
